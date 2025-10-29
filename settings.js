0646d7b5039db9eec398fc3756d83daf32dd3fbf
Yan-Kumara <yankumara@alunos.utfpr.edu.br>
2025-10-29 15:09:29 -0300

    PasswordUI

--- settings.js
+++ settings.js
@@ -1,18 +1,22 @@
+// settings.js focado em fazer o UI funcionar
+
 module.exports = {
     // Habilita a segurança do editor de fluxos
-    adminAuth: {
+    /* adminAuth: {  // Comentado por enquanto
         type: "credentials",
         users: [{
             username: process.env.NODE_RED_USERNAME,
             password: process.env.NODE_RED_PASSWORD,
             permissions: "*"
         }]
-    },
+    }, */
+    
     // HABILITA A SEGURANÇA DO DASHBOARD E OUTROS ENDPOINTS HTTP
     httpNodeAuth: {
         user: process.env.NODE_RED_HTTP_USERNAME,
         pass: process.env.NODE_RED_HTTP_PASSWORD
     },
+    
     // Garante que o Node-RED use o disco persistente do Render
     userDir: "./"
 }
