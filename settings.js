f56c4c00a4de4a49848deacfbb0e611f14b184c3
Yan-Kumara <yankumara@alunos.utfpr.edu.br>
2025-10-28 23:29:57 -0300

    LoginsAdded
    Cmon

---
+++ settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // Habilita a segurança do editor de fluxos
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: process.env.NODE_RED_USERNAME,
+            password: process.env.NODE_RED_PASSWORD,
+            permissions: "*"
+        }]
+    },
+    // HABILITA A SEGURANÇA DO DASHBOARD E OUTROS ENDPOINTS HTTP
+    httpNodeAuth: {
+        user: process.env.NODE_RED_HTTP_USERNAME,
+        pass: process.env.NODE_RED_HTTP_PASSWORD
+    },
+    // Garante que o Node-RED use o disco persistente do Render
+    userDir: "./"
+}
